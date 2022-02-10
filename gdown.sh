be0f653bc41a649b6213c20380193d8428f68c90
nshunt639 <nshunt639@gmail.com>
2022-02-10 11:42:19 -0800

    chore: update gdown.sh

--- gdown.sh
+++ gdown.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-fileid="1UxIlQPR5z2tEd7IAy4BwUzp4Vkf9PECB"
+fileid="google drive file id"
 filename="images.zip"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
